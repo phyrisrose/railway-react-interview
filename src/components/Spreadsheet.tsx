e7b4c2f52f6f5e54c48219eea5bb8d3fb1d7e959
Anna <phyrisrose@gmail.com>
2023-01-24 15:28:50 -0700

    moved the cells up and down

--- src/components/Spreadsheet.tsx
+++ src/components/Spreadsheet.tsx
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import React, { useState } from 'react';
 
 import Cell from 'components/Cell';
+import { getNewLocation } from '../util';
 
 const NUM_ROWS = 10;
 const NUM_COLUMNS = 10;
@@ -44,7 +45,41 @@ const Spreadsheet: React.FC = () => {
                     ]);
                   }}
                   onMove={(direction: string) => {
-                    console.log(`moving [${rowIdx}][${columnIdx}] to ${direction}`);
+                    const [newRowIdx, newColumnIdx] = getNewLocation(direction, rowIdx, columnIdx);
+                    console.log(
+                      `moving [${rowIdx}][${columnIdx}] to ${direction}, at [${newRowIdx}][${newColumnIdx}]`,
+                    );
+
+                    // overwrite the new location
+                    // wipe the current location
+
+                    // likely need current row, and new row. and then set two rows.
+                    // this is the new current row
+                    const newDestinationRow = [
+                      ...cellState[newRowIdx].slice(0, columnIdx),
+                      cellValue,
+                      ...cellState[newRowIdx].slice(columnIdx + 1),
+                    ];
+
+                    const newSourceRow = [
+                      ...cellState[rowIdx].slice(0, columnIdx),
+                      cellValue,
+                      ...cellState[rowIdx].slice(columnIdx + 1),
+                    ];
+
+                    // determine order of row insertion
+                    const newRows =
+                      newRowIdx < rowIdx
+                        ? [newDestinationRow, newSourceRow]
+                        : [newSourceRow, newDestinationRow];
+
+                    // now, here, we find the new start insertion row index
+                    const startInsertionIdx = newRowIdx < rowIdx ? newRowIdx : rowIdx;
+                    setCellState([
+                      ...cellState.slice(0, startInsertionIdx),
+                      ...newRows,
+                      ...cellState.slice(startInsertionIdx + 2),
+                    ]);
                   }}
                 />
               );
